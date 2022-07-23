0fff0cb3371aeace62c4df7b7a0f313d4800a950
Ginura Ransika De Seram <ginuraransika@gmail.com>
2022-07-24 03:43:39 +0530

    finished the gulp exercise 2

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,7 +3,14 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync');
-
+const del = require('del');
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+const usemin = require('gulp-usemin');
+const rev = require('gulp-rev');
+const cleanCss = require('gulp-clean-css');
+const flatMap = require('gulp-flatmap');
+const htmlmin = require('gulp-htmlmin');
 
 gulp.task('sass', function(){
     // ? gulp.src() - function that takes file globs and creates a stream of objects that represent the files
@@ -37,3 +44,41 @@ gulp.task('browser-sync', function() {
 
 // Default task
 gulp.task('default', gulp.parallel('browser-sync','sass:watch'));
+
+gulp.task('clean', function() {
+    return del(['dist/']);
+});
+
+// * copy fonts to the distribution folder 
+gulp.task('copyfonts', function(done){
+    gulp.src('./node_modules/@fortawesome/fontawesome-free/webfonts/*.*')
+    .pipe(gulp.dest('./dist/webfonts'));
+    done();
+});
+
+gulp.task('imagemin', function(done){
+    return gulp.src('img/*.{png,jpg,gif}')
+    .pipe(imagemin({optimizationLevel: 3, progressive: true, interlaced:true}))
+    .pipe(gulp.dest('dist/img'));
+});
+
+
+// * this takes html files and then loos up the CSS and JS blocks in the html files
+gulp.task('usemin', function(){
+    return gulp.src('./*.html')
+    // ? flatmap - takes multiple html files and then starts up parallel pipelines for each one of these html files.
+    .pipe(flatMap(function(stream, file){
+        return stream
+        .pipe(usemin({
+            css: [rev()], 
+            html: [function(){return htmlmin({collapseWhitespace:true})}],
+            js:[uglify(), rev()],
+            inlinejs: [uglify()],
+            inlinecss: [cleanCss(), 'concat']
+        }))
+    }))
+    .pipe(gulp.dest('dist/'));
+});
+
+
+gulp.task('build', gulp.series('clean','copyfonts','imagemin','usemin'));
